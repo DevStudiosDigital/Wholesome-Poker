ae490c2665a85cff374aa49087b4a4ca2b8a432c
andreluque <andre.luque.c@gmail.com>
2024-04-18 17:26:42 +0400

    blast points

--- contracts/wpStaking.sol
+++ contracts/wpStaking.sol
@@ -372,6 +372,52 @@ interface IERC721 is IERC165 {
     function totalSupply() external view returns (uint256);
 }
 
+enum YieldMode {
+    AUTOMATIC,
+    VOID,
+    CLAIMABLE
+}
+
+enum GasMode {
+    VOID,
+    CLAIMABLE 
+}
+
+interface IBlast{
+    // configure
+    function configureContract(address contractAddress, YieldMode _yield, GasMode gasMode, address governor) external;
+    function configure(YieldMode _yield, GasMode gasMode, address governor) external;
+
+    // base configuration options
+    function configureClaimableYield() external;
+    function configureClaimableYieldOnBehalf(address contractAddress) external;
+    function configureAutomaticYield() external;
+    function configureAutomaticYieldOnBehalf(address contractAddress) external;
+    function configureVoidYield() external;
+    function configureVoidYieldOnBehalf(address contractAddress) external;
+    function configureClaimableGas() external;
+    function configureClaimableGasOnBehalf(address contractAddress) external;
+    function configureVoidGas() external;
+    function configureVoidGasOnBehalf(address contractAddress) external;
+    function configureGovernor(address _governor) external;
+    function configureGovernorOnBehalf(address _newGovernor, address contractAddress) external;
+
+    // claim yield
+    function claimYield(address contractAddress, address recipientOfYield, uint256 amount) external returns (uint256);
+    function claimAllYield(address contractAddress, address recipientOfYield) external returns (uint256);
+
+    // claim gas
+    function claimAllGas(address contractAddress, address recipientOfGas) external returns (uint256);
+    function claimGasAtMinClaimRate(address contractAddress, address recipientOfGas, uint256 minClaimRateBips) external returns (uint256);
+    function claimMaxGas(address contractAddress, address recipientOfGas) external returns (uint256);
+    function claimGas(address contractAddress, address recipientOfGas, uint256 gasToClaim, uint256 gasSecondsToConsume) external returns (uint256);
+
+    // read functions
+    function readClaimableYield(address contractAddress) external view returns (uint256);
+    function readYieldConfiguration(address contractAddress) external view returns (uint8);
+    function readGasParams(address contractAddress) external view returns (uint256 etherSeconds, uint256 etherBalance, uint256 lastUpdated, GasMode);
+}
+
 
 contract WholeSomePokerStaking is Ownable {
 
@@ -400,6 +446,8 @@ contract WholeSomePokerStaking is Ownable {
     constructor() {
         // NFT = IERC721();
         // TOKEN = IERC20Metadata();
+
+        IBlast(0x4300000000000000000000000000000000000002).configureClaimableGas();
     }
 
     ///
@@ -575,6 +623,10 @@ contract WholeSomePokerStaking is Ownable {
         require(token != address(0), "Invalid Token!");
 
         require(IERC20(token).transfer(recipient, amount), "Unsuccessful Transfer!");
+    }
+
+    function claimMyContractsGas(address recipient) external onlyOwner {
+        IBlast(0x4300000000000000000000000000000000000002).claimAllGas(address(this), recipient);
     }
 
     
