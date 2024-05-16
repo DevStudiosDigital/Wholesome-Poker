feae8cee70243f0684517c4f1bf5db24dc1324cc
trust0212-fantohm <kishi.miracledev2@gmail.com>
2024-05-16 11:54:42 -0400

    resolve issue

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -8,35 +8,30 @@ import {
 import { getOwnedNFTsQuery } from "./apollo-query";
 
 export const getOwnedTokenIds = async (owner: string) => {
-  try {
-    const defaultOptions: DefaultOptions = {
-      watchQuery: {
-        fetchPolicy: "no-cache",
-        errorPolicy: "ignore",
-      },
-      query: {
-        fetchPolicy: "no-cache",
-        errorPolicy: "all",
-      },
-    };
-
-    const client = new ApolloClient({
-      link: new HttpLink({ uri: GraphUrl, fetch }),
-      cache: new InMemoryCache(),
-      defaultOptions,
-    });
-    const query = getOwnedNFTsQuery(owner);
-    const res = await client.query({
-      query,
+  const defaultOptions: DefaultOptions = {
+    watchQuery: {
       fetchPolicy: "no-cache",
-      errorPolicy: "none",
-    });
-    const tokenIds: number[] = res.data.tokens.map((token: any) =>
-      Number(token.tokenId)
-    );
-    return tokenIds;
-  } catch (error) {
-    console.error("[getOwnedTokenIds]: ", error);
-    return [];
-  }
+      errorPolicy: "ignore",
+    },
+    query: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "all",
+    },
+  };
+
+  const client = new ApolloClient({
+    link: new HttpLink({ uri: GraphUrl, fetch }),
+    cache: new InMemoryCache(),
+    defaultOptions,
+  });
+  const query = getOwnedNFTsQuery(owner);
+  const res = await client.query({
+    query,
+    fetchPolicy: "no-cache",
+    errorPolicy: "none",
+  });
+  const tokenIds: number[] = res.data.tokens.map((token: any) =>
+    Number(token.tokenId)
+  );
+  return tokenIds;
 };
